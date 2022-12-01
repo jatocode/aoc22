10a8748c2276654f01796d885a25f38d2a87df3a
Tobias <tobias.jansson@gmail.com>
2022-12-01 07:40:48 +0100

    dag 1 del 1

--- 01.js
+++ 01.js
@@ -1,30 +1,26 @@
-const { table } = require('console');
-const fs = require('fs');
-const args = process.argv.slice(2);
+const fs = require('fs')
+const args = process.argv.slice(2)
 
-const data = fs.readFileSync(args[0], 'utf8');
-const lines = data.split('\n').map(x => parseInt(x));
+const data = fs.readFileSync(args[0], 'utf8')
+const lines = data.split('\n')
 
-let current = parseInt(lines[0]);
-
-let p1 = 0;
+let elves = []
+let elve = 1
+let max = 0
+let maxElve = 0
 lines.forEach(x => {
-    if(x > current) p1++;
-    current = x;
-});
+    if(x.length != 0) {
+    const cal = parseInt(x)
+    elves[elve] = elves[elve] == undefined ? cal : elves[elve] + cal
+    console.log(elves[elve])
+    }
+    if (elves[elve] > max) {
+        max = elves[elve]
+        maxElve = elve
+    }
+    if (x.length === 0) elve++
+})
 
-console.log('Del 1: ' + p1);
+console.log(`Del 1: Most carrying is ${maxElve} with ${max}`)
 
-current = lines[0] + lines[1] + lines[2];
-let p2 = 0;
-for (let index = 0; index < lines.length; index++) {
-    const e1 = lines[index];
-    const e2 = lines[index+1];
-    const e3 = lines[index+2];
-
-    let win = e1+e2+e3;
-    if(win > current) p2++;
-    current = win;
-}
-
-console.log('Del 2: ' + p2);
+console.log(elves)
