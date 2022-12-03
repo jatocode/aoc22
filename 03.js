b468149d6be87ae134f5aa151101686ee746a973
Tobias <tobias.jansson@gmail.com>
2022-12-03 10:32:50 +0100

    del 2

--- 03.js
+++ 03.js
@@ -4,15 +4,26 @@ const args = process.argv.slice(2)
 const data = fs.readFileSync(args[0], 'utf8')
 const lines = data.split('\n')
 
+function prio(x) {
+    return x > 96 ? x - 96 : x - 65 + 27
+}
+
 const tot = lines.reduce((sum, l) => {
     const h1 = l.substring(0, l.length/2).split('')
     const h2 = l.substring(l.length/2).split('')
-
     const both = h1.filter(x => h2.includes(x)).join().charCodeAt(0)
-
-    prio = both > 96 ? both - 96 : both - 65 + 27
-    return sum + prio
-
+    return sum + prio(both)
 },0)
-
 console.log('Dag 3, del 1', tot)
+
+let sum = 0
+for(let i=0;i<lines.length;i+=3) {
+    const h1 = lines[i].split('')
+    const h2 = lines[i+1].split('')
+    const h3 = lines[i+2].split('')
+
+    const all = h1.filter(x => h2.includes(x)).filter(x => h3.includes(x)).join().charCodeAt(0)
+    sum += prio(all)
+}
+console.log('Dag 3, del 2', sum)
+
