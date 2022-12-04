015ce8da35dc3e5200525ada07f0a1e49e6e02b6
Tobias <tobias.jansson@gmail.com>
2022-12-04 10:36:30 +0100

    Dag 4 del 1

---
+++ 04.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const args = process.argv.slice(2)
+
+const data = fs.readFileSync(args[0], 'utf8')
+const lines = data.split('\n')
+
+let cont = 0;
+const sections = lines.map(line => {
+    const m = line.match(/(\d+)-(\d+),(\d+)-(\d+)/)
+    const s1 = {b:parseInt(m[1]), e:parseInt(m[2])}
+    const s2 = {b:parseInt(m[3]), e:parseInt(m[4])}
+
+    if(s1.b <= s2.b && s1.e >= s2.e) cont++
+    else if(s2.b <= s1.b && s2.e >= s1.e) cont++
+
+    return {s1:s1, s2:s2}
+})
+
+console.log('Dag 1', cont)
