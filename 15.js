2f0ffcf7d7e94fc43371e1e74ea6cbfc6120b28b
Tobias Jansson <tobias.jansson@prevas.se>
2022-12-15 10:03:44 +0100

    dag 15 almost

--- 15.js
+++ 15.js
@@ -4,32 +4,47 @@ const args = process.argv.slice(2)
 const lines = osfs.readFileSync(args[0], 'utf8').split('\n')
 
 const grid = {}
-lines.forEach(line => {
-    const m = line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
-    const sx = parseInt(m[1])
-    const sy = parseInt(m[2])
-    const bx = parseInt(m[3])
-    const by = parseInt(m[3])
+const checkrow = 10 // 200000
+plot(checkrow)
+//print()
 
-    const dist = Math.abs(sx - bx) + Math.abs(sy - by)
-    console.log(sx, sy, bx, by, dist)
-
-    for (let y = sy - dist; y < sy + dist; y++) {
-        for (let x = sx - dist; x < sx + dist; x++) {
-            if (Math.abs(x - sx) + Math.abs(y - sy) <= dist) {
-                grid[[x, y]] = '#'
-            }            
-        }
-    }
+// Y-koordinat  som inte har en beacon
+const keysNoBeacon = Object.keys(grid).filter(x => {
+    y = parseInt(x.split(',')[1])
+    return y === checkrow && grid[x] !== 'B'
 })
+//console.log(keys10.reduce((a, b) => a + grid[b], ''))
+console.log('Dag 15, del 1:', keysNoBeacon.length)
 
-print()
+function plot(checkrow) {
+    lines.forEach(line => {
+        const m = line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)
+        const sx = parseInt(m[1])
+        const sy = parseInt(m[2])
+        const bx = parseInt(m[3])
+        const by = parseInt(m[4])
+
+        const dist = Math.abs(sx - bx) + Math.abs(sy - by) + 1
+        
+        console.log({sx, sy, bx, by, dist})
+
+        const y = checkrow
+        for (let x = sx - dist - 1; x < sx + dist + 1; x++) {
+            if (x == bx && y == by) grid[[x, y]] = 'B'
+            if (Math.abs(x - sx) + Math.abs(y - sy) < dist) {
+                if (x == sx && y == sy) grid[[x, y]] = 'S'
+                else if (grid[[x, y]] === undefined) grid[[x, y]] = '#'
+            }
+        }
+
+    })
+}
 
 function print() {
-    for (let y = 0; y < 20; y++) {
+    for (let y = 8; y < 12; y++) {
         let line = ''
-        for (let x = -20; x < 40; x++) {
-            line += grid[[x,y]] || '.'
+        for (let x = -10; x < 30; x++) {
+            line += grid[[x, y]] || '.'
         }
         console.log(line)
     }
