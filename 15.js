d122d8284cd6ead610839d20967fbb5fa957b769
Tobias Jansson <tobias.jansson@prevas.se>
2022-12-15 11:32:30 +0100

    Dag 15 del 1

--- 15.js
+++ 15.js
@@ -4,16 +4,16 @@ const args = process.argv.slice(2)
 const lines = osfs.readFileSync(args[0], 'utf8').split('\n')
 
 const grid = {}
-const checkrow = 10 // 200000
+const checkrow = 2000000
 plot(checkrow)
 //print()
 
 // Y-koordinat  som inte har en beacon
+console.log('Done plotting, checking row:', checkrow)
 const keysNoBeacon = Object.keys(grid).filter(x => {
     y = parseInt(x.split(',')[1])
     return y === checkrow && grid[x] !== 'B'
 })
-//console.log(keys10.reduce((a, b) => a + grid[b], ''))
 console.log('Dag 15, del 1:', keysNoBeacon.length)
 
 function plot(checkrow) {
@@ -25,18 +25,16 @@ function plot(checkrow) {
         const by = parseInt(m[4])
 
         const dist = Math.abs(sx - bx) + Math.abs(sy - by) + 1
-        
-        console.log({sx, sy, bx, by, dist})
+        console.log({sx, sy, dist})
 
         const y = checkrow
         for (let x = sx - dist - 1; x < sx + dist + 1; x++) {
             if (x == bx && y == by) grid[[x, y]] = 'B'
             if (Math.abs(x - sx) + Math.abs(y - sy) < dist) {
-                if (x == sx && y == sy) grid[[x, y]] = 'S'
-                else if (grid[[x, y]] === undefined) grid[[x, y]] = '#'
+//                if (x == sx && y == sy) grid[[x, y]] = 'S'
+                if (grid[[x, y]] === undefined) grid[[x, y]] = '#'
             }
         }
-
     })
 }
 
